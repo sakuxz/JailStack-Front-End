c48c59ed1d9b5e2e7109da58c19440fb9db2e58b
yoychen <yui12327@gmail.com>
2018-01-05 00:26:26 +0800

    add redux-thunk sample

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -3,20 +3,47 @@ import React from 'react';
 import { push } from 'react-router-redux';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import {
+  increment,
+  incrementAsync,
+  decrement,
+  decrementAsync,
+} from '../../modules/counter';
 
 const Home = props => (
   <div>
     <h1>Home</h1>
-    <p>Welcome home!</p>
+    <p>Count: {props.count}</p>
+
+    <p>
+      <button onClick={props.increment} disabled={props.isIncrementing}>Increment</button>
+      <button onClick={props.incrementAsync} disabled={props.isIncrementing}>Increment</button>
+    </p>
+
+    <p>
+      <button onClick={props.decrement} disabled={props.isDecrementing}>decrement</button>
+      <button onClick={props.decrementAsync} disabled={props.isDecrementing}>decrement</button>
+    </p>
+
     <button onClick={() => props.changePage()}>Go to about page via redux</button>
   </div>
 );
 
+const mapStateToProps = state => ({
+  count: state.counter.count,
+  isIncrementing: state.counter.isIncrementing,
+  isDecrementing: state.counter.isDecrementing,
+});
+
 const mapDispatchToProps = dispatch => bindActionCreators({
+  increment,
+  incrementAsync,
+  decrement,
+  decrementAsync,
   changePage: () => push('/about-us'),
 }, dispatch);
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps,
 )(Home);
